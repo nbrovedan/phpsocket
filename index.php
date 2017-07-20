6b2c162d8e6f9b7a41d60f7e1738303910855501
Naidion Concencio Brovedan <naidion.brovedan@brognoli.com.br>
2017-07-20 11:04:59 -0300

    Adding

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
-	//error_reporting(E_ERROR | E_PARSE);
-	
 	require 'vendor/autoload.php';
-		
+	
+	//Cria o objeto
 	$audio = new Audio();
+	//Cria a mensagem
 	$message = $audio->createMessage('489998484701','https://www.google.com/audio.mp3',true);
+	//Envia a mensagem
 	print_r($audio->sendVoiceMessage($message));
+	//Busca a mensagem 1
+	print_r($audio->getVoiceMessage(1));
 ?>
